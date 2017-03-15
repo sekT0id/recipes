544400b201671052e808c86a8ba745f25fc8d981
Alex Prusov <Alex Prusov>
2017-03-15 19:26:15 +0300

    CRUD for ingredients added

---
+++ backend/views/ingredients/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\IngredientsSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Ингредиенты';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="ingredients-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Добавить ингредиент', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin();?>
+        <?php echo GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                'id',
+                'name:ntext',
+                'status',
+
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]);?>
+    <?php Pjax::end();?>
+</div>
