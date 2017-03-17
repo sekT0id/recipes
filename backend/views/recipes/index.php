8fd5c52d6bee6c7fc0a8466f39c1ed708ca71c50
Alex Prusov <Alex Prusov>
2017-03-17 15:07:04 +0300

    cls

--- backend/views/recipes/index.php
+++ backend/views/recipes/index.php
@@ -1,8 +1,11 @@
 <?php
 
 use yii\helpers\Html;
+
 use yii\grid\GridView;
+
 use yii\widgets\Pjax;
+
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\RecipesSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
