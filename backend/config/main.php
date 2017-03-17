36c4b09d04265da582aa9bb70ba865dc603fc991
Alex Prusov <Alex Prusov>
2017-03-17 18:16:44 +0300

    base url fixed

--- backend/config/main.php
+++ backend/config/main.php
@@ -14,6 +14,7 @@ return [
     'modules' => [],
     'components' => [
         'request' => [
+            'baseUrl' => '',
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
